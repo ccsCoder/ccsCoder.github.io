1c467c216dda7a5930ac1cd3e9287dce0938df2c
ccsCoder <ccsCoder@gmail.com>
2015-09-17 10:33:18 +0530

    Added description for Projects and other places. implemented smooth scroll.

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,14 @@ $(document).ready(function() {
 	$(".experiment_item").on("click",function(event) {
 		window.open($(this).attr("link_to_follow"),"_blank");
 	});
+	$(".topNavItem").on("click",function(event) {
+		scrollToTarget("#"+$(this).attr("scrollTarget"));
+	});
 });
 
+
+function scrollToTarget(scrollTarget) {
+    $('html, body').animate({
+        scrollTop: $(scrollTarget).offset().top
+    }, 2000);
+}
